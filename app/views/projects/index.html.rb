6b46f0de28fe5228a09eada6adf6f4ab6159142b
GodoyKarina <107443505+GodoyKarina@users.noreply.github.com>
2023-09-11 22:30:56 +0000

    Projects cards

---
+++ app/views/projects/index.html.rb
@@ -0,0 +1,28 @@
+<h1>Mis proyectos</h1>
+
+<div class="card">
+  <img src="<%= asset_path('card-1.png') %>" class="card-img-top" alt="Proyecto 1">
+  <div class="card-body">
+    <h5 class="card-title">Proyecto 1</h5>
+    <p class="card-text">Proyecto Viajes Chile.</p>
+    <a href="#" class="btn btn-primary">Ver más</a>
+  </div>
+</div>
+
+<div class="card">
+  <img src="<%= asset_path('card-2.png') %>" class="card-img-top" alt="Proyecto 2">
+  <div class="card-body">
+    <h5 class="card-title">Proyecto 2</h5>
+    <p class="card-text">Proyecto Ricomida.</p>
+    <a href="#" class="btn btn-primary">Ver más</a>
+  </div>
+</div>
+
+<div class="card">
+  <img src="<%= asset_path('card-3.png') %>" class="card-img-top" alt="Proyecto 3">
+  <div class="card-body">
+    <h5 class="card-title">Proyecto 3</h5>
+    <p class="card-text">Proyecto Cuppon Page.</p>
+    <a href="#" class="btn btn-primary">Ver más</a>
+  </div>
+</div>
